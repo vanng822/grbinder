349dcea40a8ead25391dcf072c76a7fe8f0a0604
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2025-09-15 17:13:34 +0200

    Possibility to lock when binding to verb like POST, GET

--- grbinder.go
+++ grbinder.go
@@ -4,8 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Mix runs BindVerb and CRUB. One has to make sure no collision!
-func Mix(group *gin.RouterGroup, handler any) {
-	BindVerb(group, handler)
-	CRUD(group, handler)
+// Mix runs BindVerb and CRUD. One has to make sure no collision!
+// when using entity lock with own entity id lookup func
+// one has to make sure the entity id lookup func works
+// if it returns empty string, no lock will be applied
+func Mix(group *gin.RouterGroup, handler any, options ...Option) {
+	BindVerb(group, handler, options...)
+	CRUD(group, handler, options...)
 }
